13dadb684643bd4a9b903467cdc2502e25ac18e3
Tong Tang <pichip22@hotmail.com>
2023-06-25 19:25:56 -0400

    install react-router-dom

--- src/components/App.js
+++ src/components/App.js
@@ -1,24 +1,42 @@
+import React, {useState} from 'react';
+import { BrowserRouter as Router,Switch,Route,Link } from 'react-router-dom';
 import logo from '../logo.svg';
 import '../App.css';
+import './Home';
 
 function App() {
+  //Navigation
+  const [page,setPage] = useState("List");
+  
   return (
-    <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
-    </div>
+    <Router>
+
+      <div className="App">
+        <nav>
+          <ul>
+            
+              <Link to="/">Home </Link>
+              <Link to="/About"> About</Link>
+            
+          </ul>
+        </nav>
+        <header className="App-header">
+          <img src={logo} className="App-logo" alt="logo" />
+          <p>
+            Super fat.
+          </p>
+          <a
+            className="App-link"
+            href="https://yeah1tnt.github.io/"
+            target="_blank"
+            rel="noopener noreferrer"
+          >
+            Learn React
+          </a>
+        </header>
+      </div>
+
+    </Router>
   );
 }
 
