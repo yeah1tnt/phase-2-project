1c4c0d99f30ff427ba955a88581a94838f7b44c1
Tong Tang <pichip22@hotmail.com>
2023-07-02 18:54:49 -0400

    removed logo

--- src/components/App.js
+++ src/components/App.js
@@ -1,6 +1,5 @@
 import React from 'react';
 import { BrowserRouter as Router,Routes, Route,Link } from 'react-router-dom';
-import logo from '../logo.svg';
 import '../App.css';
 
 import HomePage from './Home';
